8223d976d0a89125cf9e0c1796ae3e7e77841a84
adamw523 <adamw523@gmail.com>
2007-04-02 16:12:53 +0000

    added nearby_intersection - contributed by Andrew Turner
    git-svn-id: http://ruby-geonames.googlecode.com/svn/trunk@7 7923239d-472a-0410-b534-77db8d43d143

--- lib/main.rb
+++ lib/main.rb
@@ -1,6 +1,7 @@
 #=============================================================================
 #
 # Copyright 2007 Adam Wisniewski <adamw@tbcn.ca> 
+# Contributions by Andrew Turner
 #
 # Licensed under the Apache License, Version 2.0 (the "License"); you may not
 # use this file except in compliance with the License. You may obtain a copy of
