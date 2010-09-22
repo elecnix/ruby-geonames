007d3c51143d1468a4f6debe67a3e4bd9f40bdb2
Chris Griego <cgriego@gmail.com>
2010-09-21 20:47:57 -0500

    Add testing infrastructure

---
+++ Rakefile
@@ -0,0 +1,13 @@
+require 'bundler/setup'
+Bundler.require(:default, :development)
+require 'rspec/core/rake_task'
+
+task :default => :spec
+
+RSpec::Core::RakeTask.new
+
+namespace :spec do
+  RSpec::Core::RakeTask.new :rcov do |task|
+    task.rcov = true
+  end
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,25 @@
+require 'rubygems'
+
+# Set up gems listed in the Gemfile.
+gemfile = File.expand_path('../Gemfile', File.dirname(__FILE__))
+begin
+  ENV['BUNDLE_GEMFILE'] = gemfile
+  require 'bundler'
+  Bundler.setup
+rescue Bundler::GemNotFound => e
+  STDERR.puts e.message
+  STDERR.puts "Try running `bundle install`."
+  exit!
+end
+
+Bundler.require(:default, :development)
+
+Rspec.configure do |config|
+  config.mock_with :rspec
+end
+
+# Requires supporting files with custom matchers and macros, etc,
+# in ./support/ and its subdirectories.
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f }
+
+require 'geonames'
